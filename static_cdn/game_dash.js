906fc76fa9401dbb41cc598da7927acd8d1b8962
Pawarit Chayawiwatkul <pawaritpawaritts@Pawarits-MacBook-Air-2.local>
2023-05-20 20:11:18 +0700

    Fixed WSGI

--- static_cdn/game_dash.js
+++ static_cdn/game_dash.js
@@ -1,138 +1,319 @@
-
 const jsdata = JSON.parse(document.getElementById('jsdata').textContent);
-function BarChart(query, data) {
-    console.log(data)
 
+function degToRad(degrees) {
+  return degrees * Math.PI / 180;
+}
+function LineChart(query, xlabel, series) {
     var options = {
-          series: [
-            // {
-            //     name: 'LastGame',
-            //     data: data.lastgame,
-            // }, 
-            {
-              name: 'CurrentGame',
-              data: data
-            }, 
-            
-        ],
-            chart: {
-            type: 'bar',
-            height: 400,
-            
-            toolbar: {
-                show: false,
-            },
-            
-        },
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '57%',
-            endingShape: "rounded",
-            borderRadius: 12,
+        series: series,
+          chart: {
+          type: 'line',
+          height: 300,
+          toolbar: {
+              show: false,
           },
           
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '70%',
+          endingShape: 'rounded'
         },
-        states: {
-          hover: {
-            filter: 'none',
-          }
-        },
-        colors:['#FFA26D', '#FF5ED2'],
-        dataLabels: {
-          enabled: false,
-        },
-        markers: {
-    shape: "circle",
-    },
-    
-    
-        legend: {
-            show: false,
-            fontSize: '12px',
-            labels: {
-                colors: '#000000',
-                
-                },
-            markers: {
-            width: 18,
-            height: 18,
-            strokeWidth: 10,
-            strokeColor: '#fff',
-            fillColors: undefined,
-            radius: 12,	
-            }
-        },
-        stroke: {
+      },
+      colors:['#886CC0', '#FFEC4B', '#61CFF1', '#19C4FF'],
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+          shape: "circle",
+      },
+
+      legend: {
+          show: false,
+      },
+      stroke: {
+        show: true,
+        width: 10,
+        curve:'smooth',
+        colors:['var(--primary)', '#FFEC4B', '#61CFF1', '#19C4FF'],
+      },
+      
+      grid: {
+          borderColor: '#eee',
           show: true,
-          width: 4,
-          curve: 'smooth',
-          lineCap: 'round',
-          colors: ['transparent']
-        },
-        grid: {
-            borderColor: '#eee',
-        },
-        xaxis: {
-             position: 'bottom',
-          categories: ['Rotate', 'HandUpDown', 'TipToe', 'Angry', 'Happy', 'Sad'],
-          labels: {
-           style: {
-              colors: '#787878',
+           xaxis: {
+              lines: {
+                  show: true,
+              }
+          },  
+           yaxis: {
+              lines: {
+                  show: false,
+              }
+          },  
+      },
+      xaxis: {
+          
+        categories: xlabel,
+        labels: {
+          style: {
+              colors: '#7E7F80',
               fontSize: '13px',
-              fontFamily: 'poppins',
+              fontFamily: 'Poppins',
               fontWeight: 100,
               cssClass: 'apexcharts-xaxis-label',
-            },
-          },
-          crosshairs: {
-          show: false,
-          }
-        },
-        yaxis: {
-            labels: {
-                offsetX:-16,
-               style: {
-                  colors: '#787878',
-                  fontSize: '13px',
-                   fontFamily: 'poppins',
-                  fontWeight: 100,
-                  cssClass: 'apexcharts-xaxis-label',
-              },
           },
         },
-        fill: {
-            type: 'gradient',
-            gradient: {
-                shade: 'white',
-                type: "vertical",
-                shadeIntensity: 0.2,
-                gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-                inverseColors: true,
-                opacityFrom: 1,
-                opacityTo: 1,
-                stops: [0, 50, 50],
-                colorStops: []
-            }
-        }, 
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return val 
-            }
-          }
+        crosshairs: {
+        show: false,
+        }
+      },
+      yaxis: {
+      show:true,	
+      labels: {
+          offsetX: -15,
+         style: {
+            colors: '#7E7F80',
+            fontSize: '14px',
+             fontFamily: 'Poppins',
+            fontWeight: 100,
+            
+          },
+           formatter: function (y) {
+                    return y;
+                  }
         },
-        };
+      },
+      fill: {
+        opacity: 1,
+        colors:'#FAC7B6'
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val
+          }
+        }
+      }
+      };
 
-        var chartBar1 = new ApexCharts(document.querySelector(query), options);
-        // chartBar1.render();
-        setTimeout(chartBar1.render(), 2000); 
-        console.log('rendered')
+      var chart1 = new ApexCharts(document.querySelector(query), options);
+      setTimeout(chart1.render(), 2000); 
+   
 }
-
 
 function RevenueGraph (data) {
-    BarChart('#gamegraph1', data);
+    var rotate = [];
+    var und = [];
+    var angry = [];
+    var sad = [];
+    var smile = [];
+    var tiptoe = [];
+    var index = [];
+    for (let i = 0; i < data.length; i++) {
+        rotate.push(data[i].rotate);
+        und.push(data[i].hand);
+        angry.push(data[i].angry);
+        sad.push(data[i].sad);
+        smile.push(data[i].smile);
+        tiptoe.push(data[i].tiptoe);
+        index.push(i + 1);
+      }
+    LineChart('#gamegraph1', index, 
+    [{
+        name: 'Rotate',
+        data: rotate,
+        //radius: 12,	
+    }, {
+        name: 'Up And Down',
+        data: und,
+        //radius: 12,	
+    }]);
+    LineChart('#gamegraph2', index, 
+    [{
+        name: 'Angry',
+        data: angry,
+        //radius: 12,	
+    }, {
+        name: 'Smile',
+        data: smile,
+        //radius: 12,	
+    },  {
+        ndame: 'Sad',
+        data: sad,
+        //raius: 12,	
+    }]);
+    LineChart('#gamegraph3', index, 
+    [{
+        name: 'Tip Toe',
+        data: tiptoe,
+        //radius: 12,	
+    }],
+    )
 }
 
-RevenueGraph(jsdata);
+function LineChart(query, xlabel, series) {
+  var options = {
+      series: series,
+        chart: {
+        type: 'line',
+        height: 300,
+        toolbar: {
+            show: false,
+        },
+        
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '70%',
+        endingShape: 'rounded'
+      },
+    },
+    colors:['#886CC0', '#FFEC4B', '#61CFF1', '#19C4FF'],
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+        shape: "circle",
+    },
+
+    legend: {
+        show: false,
+    },
+    stroke: {
+      show: true,
+      width: 5,
+      curve:'smooth',
+      colors:['var(--primary)', '#FFEC4B', '#61CFF1', '#19C4FF'],
+    },
+    
+    grid: {
+        borderColor: '#eee',
+        show: true,
+         xaxis: {
+            lines: {
+                show: true,
+            }
+        },  
+         yaxis: {
+            lines: {
+                show: false,
+            }
+        },  
+    },
+    xaxis: {
+        
+      categories: xlabel,
+      labels: {
+        style: {
+            colors: '#7E7F80',
+            fontSize: '13px',
+            fontFamily: 'Poppins',
+            fontWeight: 100,
+            cssClass: 'apexcharts-xaxis-label',
+        },
+      },
+      crosshairs: {
+      show: false,
+      }
+    },
+    yaxis: {
+    show:true,	
+    labels: {
+        offsetX: -15,
+       style: {
+          colors: '#7E7F80',
+          fontSize: '14px',
+           fontFamily: 'Poppins',
+          fontWeight: 100,
+          
+        },
+         formatter: function (y) {
+                  return y;
+                }
+      },
+    },
+    fill: {
+      opacity: 1,
+      colors:'#FAC7B6'
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val
+        }
+      }
+    }
+    };
+
+    var chart1 = new ApexCharts(document.querySelector(query), options);
+    setTimeout(chart1.render(), 2000); 
+ 
+}
+
+function drawSpotGraph(data) {
+  udata = JSON.parse(data['daily'])
+  console.log(udata)
+  var salinity = [];
+  var waterlevel = [];
+  var watervelocity = [];
+  var tempreture = [];
+  var index = [];
+  for (let i = 0; i < udata.length; i++) {
+    salinity.push(udata[i].salinity);
+    waterlevel.push(udata[i].waterlevel);
+    watervelocity.push(udata[i].watervelocity);
+    tempreture.push(udata[i].tempreture);
+    index.push(i + 1);
+  }
+  LineChart(`#${data['srnumber']}1`, index, 
+    [{
+        name: 'Salinity',
+        data: salinity.slice(0, 7),
+        //radius: 12,	
+    }, {
+      name: 'Water Level',
+      data: waterlevel.slice(0, 7)
+    }, {
+      name: 'Water Velocity',
+      data: watervelocity.slice(0, 7)
+    }, {
+      name: 'Tempreture',
+      data: tempreture.slice(0, 7)
+    }]);
+  udata = JSON.parse(data['monthly'])
+  var salinity = [];
+  var waterlevel = [];
+  var watervelocity = [];
+  var tempreture = [];
+  var index = [];
+  for (let i = 0; i < udata.length; i++) {
+    salinity.push(udata[i].salinity);
+    waterlevel.push(udata[i].waterlevel);
+    watervelocity.push(udata[i].watervelocity);
+    tempreture.push(udata[i].tempreture);
+    index.push(i + 1);
+  }
+  LineChart(`#${data['srnumber']}2`, index, 
+    [{
+        name: 'Salinity',
+        data: salinity,
+        //radius: 12,	
+    }, {
+      name: 'Water Level',
+      data: waterlevel
+    }, {
+      name: 'Water Velocity',
+      data: watervelocity
+    }, {
+      name: 'Tempreture',
+      data: tempreture
+    }]);
+}
+
+bouys = jsdata.bouys
+for (let i = 0; i < bouys.length; i++) {
+  drawSpotGraph(bouys[i]);
+}
--- static_cdn/patient_dash.js
+++ static_cdn/patient_dash.js
@@ -1,26 +1,78 @@
 const jsdata = JSON.parse(document.getElementById('jsdata').textContent);
-
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext("2d");
-
+const pi2 = Math.PI * 2;
+const radius = 3;
+ctx.canvas.height = 400
 width = canvas.width
 height = canvas.height
-scale = (height / 1.3802787818178697)
-xpad = (width - (0.24695126720260419 * scale)) / 2
-console.log(12)
+scale = (height / .5)
+xpad = -60
+console.log(jsdata)
+$('#canvas').attr("width",$(window).width(width));
+$('#canvas').attr("height",$(window).height(height));
+
+vals = JSON.parse(jsdata['river']).map(x => parseFloat(x));
+
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
+}
+
+function AgriscalePoint(coor) {
+  return [height - (coor[1] - 13.5321604836276) * scale + 25, ((coor[0] - 100.2244141393265) * scale) + xpad - 25]
+}
+
+function drawAgriPolygon(points){
+  // points = [[100, 100], [100, 200], [200, 200], [200, 100]]
+  ctx.fillStyle = '#2a5e28';
+  ctx.beginPath();
+  ctx.moveTo(...AgriscalePoint(points[0]));
+  for( let i=0, l=points.length - 1; i < l; i++ ) {
+    p = AgriscalePoint(points[i + 1]);
+    ctx.lineTo(...p);
+  }
+  ctx.closePath();
+  ctx.fill();
+}
+
+function drawAgricultureArea(){
+  ctx.fillStyle = '#2a5e28';
+  ctx.beginPath();
+  console.log("AGRI")
+  r = 1
+  for( let i=0, l=AGRI_AREA.length; i < l; i++ ) {
+    drawAgriPolygon(AGRI_AREA[i]);
+      // p = scalePoint(AGRI_AREA[i][0]),
+      // ctx.save();
+      // ctx.translate( p[0] + r, p[1] );
+      // ctx.moveTo( 0, 0 );
+      // ctx.arc( 0, 0, r, 0, pi2 );
+      // ctx.restore();
+  }
+
+  ctx.fill();
 }
 function LineChart(query, xlabel, series) {
     var options = {
         series: series,
           chart: {
           type: 'line',
-          height: 300,
+          height: 150,
           toolbar: {
               show: false,
           },
-          
+          scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    userCallback: function(label, index, labels) {
+                        // when the floored value is the same as the value we have a whole number
+                        return Math.floor(label)
+   
+                    },
+                }
+            }],
+        },
       },
       plotOptions: {
         bar: {
@@ -89,7 +141,7 @@ function LineChart(query, xlabel, series) {
             
           },
            formatter: function (y) {
-                    return y;
+                    return Math.round(y * 100) / 100;
                   }
         },
       },
@@ -97,14 +149,21 @@ function LineChart(query, xlabel, series) {
         opacity: 1,
         colors:'#FAC7B6'
       },
-      tooltip: {
+      scales: {
         y: {
-          formatter: function (val) {
-            return val
-          }
-        }
+          ticks: {
+            stepSize: 1
+          },
+        },
+      },
+      // tooltip: {
+      //   y: {
+      //     formatter: function (val) {
+      //       return val;
+      //     }
+      //   }
+      // }
       }
-      };
 
       var chart1 = new ApexCharts(document.querySelector(query), options);
       setTimeout(chart1.render(), 2000); 
@@ -163,15 +222,251 @@ function RevenueGraph (data) {
 function scalePoint(coor) {
   return [((coor[0] - 100.2244141393265) * scale) + xpad, height - (coor[1] - 13.5321604836276) * scale]
 }
-RevenueGraph(jsdata.actana);
 
-console.log('h')
-ctx.lineWidth = 5
-ctx.strokeStyle = "#886CC0"; 
-point = scalePoint(BASE_RIVER[0]);
-ctx.moveTo(...point);
-for (let i = 1; i < 10; i++) {
-  console.log(i);
-  ctx.lineTo(...scalePoint(BASE_RIVER[i]));
+function drawWhiteRiver(river) {
+  ctx.lineWidth = 6;
+  point = scalePoint(river[0]);
+  ctx.moveTo(...point);
+  ctx.strokeStyle = `rgba(255, 255, 255)`; 
+  ctx.beginPath();
+  let q = 0
+  for (let i = 0; i < river.length; i++) {
+    ctx.lineTo(...scalePoint(river[i]));
+  }
+  ctx.stroke();
 }
-ctx.stroke();
+
+function drawRiver(river, value) {
+  ctx.lineWidth = 6;
+  point = scalePoint(river[0]);
+  ctx.moveTo(...point);
+  markpoints = []
+  for (let i = 0; i < Math.floor(river.length/10); i++) {
+    markpoints.push(river[i * 10])
+  }
+  markpoints = markpoints.slice(6).slice(0, 6)
+  let q = 0
+  if (value[q] < 0.25)
+    ctx.strokeStyle = `rgba(52, 134, 235)`;
+  else if (value[q] < 2)
+    ctx.strokeStyle = `rgba(235, 235, 52)`;
+  else
+    ctx.strokeStyle = `rgba(235, 52, 52)`;
+  ctx.beginPath();
+  for (let i = 0; i < river.length; i++) {
+    ctx.lineTo(...scalePoint(river[i]));
+    if (q < markpoints.length) {
+      if ((river[i][0] == markpoints[q][0]) & (river[i][1] == markpoints[q][1])) {
+        console.log('here')
+        ctx.stroke();
+        if (value[q] < 0.25)
+          ctx.strokeStyle = `rgba(52, 134, 235)`;
+        else if (value[q] < 2)
+          ctx.strokeStyle = `rgba(235, 235, 52)`;
+        else
+          ctx.strokeStyle = `rgba(235, 52, 52)`;
+        ctx.beginPath();
+        ctx.lineTo(...scalePoint(river[i]));
+        q += 1
+      }
+    }
+  }
+  ctx.stroke();
+}
+
+function drawSpread(coors) {
+  ctx.fillStyle = 'rgba(52, 134, 235)';
+  ctx.beginPath();
+  console.log(coors[0])
+  for( let i=0, l=coors.length; i < l; i++ )
+  {
+      p = scalePoint(coors[i]),
+      ctx.save();
+      ctx.translate( p[0] + radius, p[1] );
+      ctx.moveTo( 0, 0 );
+      ctx.arc( 0, 0, radius, 0, pi2 );
+      ctx.restore();
+  }
+
+  ctx.fill();
+  
+}
+
+function drawSpot(coor, yes) {
+  coor1 = scalePoint(coor)
+  console.log(coor)
+  yes = 1
+  if (yes == 0) {
+    ctx.strokeStyle = 'rgb(60, 60, 61)';
+    ctx.fillStyle = "#48FF00";
+    ctx.beginPath();
+    ctx.rect(coor1[0] - 6, coor1[1] - 6, 12, 12);
+    ctx.fill();
+  } else if (yes == 1) {
+    ctx.strokeStyle = 'rgb(60, 60, 61)';
+    ctx.fillStyle = "#FF5252";
+    ctx.beginPath();
+    ctx.rect(coor1[0] - 6, coor1[1] - 6, 12, 12);
+    ctx.fill();
+  }
+}
+
+base_image = new Image();
+base_image.src = '/static/map.png';
+base_image.onload = function(){
+  ctx.drawImage(base_image, 0, 0, width, height);
+  drawSpread(jsdata['spread_point'])
+  drawAgricultureArea()
+  drawWhiteRiver(BASE_RIVER);
+  drawRiver(BASE_RIVER, vals);
+  spots = jsdata.spots
+  for (let i = 0; i < spots.length; i++) {
+    // drawSpot([spots[i].lon, spots[i].lat], spots[i].flood)
+    console.log(spots[i]['salinity'])
+    if (i == 0){
+    drawSpotGraph(spots[i]);
+    }
+  }
+}
+
+// drawRiver(BASE_RIVER, [2, 2, 1, 0.4, 0.2, 0.1]);
+// drawRiver(RIVERS[0], 10);
+// drawRiver(RIVERS[1], 20);
+// drawRiver(RIVERS[2], 20);
+
+
+function LineChart(query, xlabel, series) {
+  var options = {
+      series: series,
+        chart: {
+        type: 'line',
+        height: 300,
+        toolbar: {
+            show: false,
+        },
+        
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '70%',
+        endingShape: 'rounded'
+      },
+    },
+    colors:['#886CC0', '#FFEC4B', '#61CFF1', '#19C4FF'],
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+        shape: "circle",
+    },
+
+    legend: {
+        show: false,
+    },
+    stroke: {
+      show: true,
+      width: 5,
+      curve:'smooth',
+      colors:['var(--primary)', '#FFEC4B', '#61CFF1', '#19C4FF'],
+    },
+    
+    grid: {
+        borderColor: '#eee',
+        show: true,
+         xaxis: {
+            lines: {
+                show: true,
+            }
+        },  
+         yaxis: {
+            lines: {
+                show: false,
+            }
+        },  
+    },
+    xaxis: {
+        
+      categories: xlabel,
+      labels: {
+        style: {
+            colors: '#7E7F80',
+            fontSize: '13px',
+            fontFamily: 'Poppins',
+            fontWeight: 100,
+            cssClass: 'apexcharts-xaxis-label',
+        },
+      },
+      crosshairs: {
+      show: false,
+      }
+    },
+    yaxis: {
+    show:true,	
+    labels: {
+        offsetX: -15,
+       style: {
+          colors: '#7E7F80',
+          fontSize: '14px',
+           fontFamily: 'Poppins',
+          fontWeight: 100,
+          
+        },
+         formatter: function (y) {
+                  return y;
+                }
+      },
+    },
+    fill: {
+      opacity: 1,
+      colors:'#FAC7B6'
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val
+        }
+      }
+    }
+    };
+
+    var chart1 = new ApexCharts(document.querySelector(query), options);
+    setTimeout(chart1.render(), 2000); 
+ 
+}
+
+function drawSpotGraph(data) {
+  udata = data['salinity']
+  var as = [];
+  var ps = [];
+  var index = [];
+  for (let i = 0; i < udata.a.length; i++) {
+    index.push(i + 1);
+  }
+  LineChart(`#${data['name']}1`, index, 
+    [{
+      name: 'Colected Value',
+      data: udata.a
+    }, {
+      name: 'Predicted Value',
+      data: udata.p,
+      //radius: 12,	
+  }]);
+  udata = data['waterlevel']
+  console.log(udata)
+  var as = [];
+  var ps = [];
+  var index = [];
+  for (let i = 0; i < udata.a.length; i++) {
+    index.push(i + 1);
+  }
+  LineChart(`#${data['name']}2`, index, 
+    [{
+      name: 'Colected Value',
+      data: udata.a
+    }, {
+        name: 'Predicted Value',
+        data: udata.p,
+        //radius: 12,	
+    }, ]);
+}
